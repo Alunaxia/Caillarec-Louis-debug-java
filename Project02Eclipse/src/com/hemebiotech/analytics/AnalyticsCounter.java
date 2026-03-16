b2424c2d7ed87bee9ed1daaea323bed4e894646b
Alunaxia <louis.caillarec@gmail.com>
2026-03-16 14:50:44 +0100

    Etape 2 correction AnalyticsCounter

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -8,39 +8,42 @@ public class AnalyticsCounter {
 	private static int headacheCount = 0;
 	private static int rashCount = 0;
 	private static int pupilCount = 0;
-	
-	public static void main(String args[]) throws Exception {
-		BufferedReader reader = new BufferedReader(
-				new FileReader("./symptoms.txt"));
-		String line = reader.readLine();
 
-		int i = 0;
-		int headCount = 0;
-		while (line != null) {
-			i++;
-			System.out.println("symptom from file: " + line);
-			if (line.equals("headache")) {
-				headCount++;
+	public static void main(String args[]) {
+		try (
+				FileWriter writer = new FileWriter("./result.out");
+				BufferedReader reader = new BufferedReader(new FileReader("./symptoms.txt"));
+				/**
+				 * Try with ressources here to handle the case if the exception happens and to still be able to close it.
+				 */
+				){
+			String line = reader.readLine();
+			while (line != null) {
+				System.out.println("symptom from file: " + line);
+				if (line.equals("headache")) {
+					headacheCount++;
+					/**
+					 * Correction to add an incrementation on the variable used to write on the file. Remove the other variable useless here.
+					 */
+				} else if (line.equals("rash")) {
+					/**
+					 * Correction of rush by rash that explains why nothing was found.
+					 */
+					rashCount++;
+				} else if (line.contains("pupils")) {
+					pupilCount++;
+				}
 
-				headacheCount++; // Correction : incrémentation de la valeur
-				System.out.println("number of headaches: " + headCount);
+				line = reader.readLine();
 			}
-			else if (line.equals("rash")) { // Correction d'une faute de frappe rush pour rash
-				rashCount++;
-			}
-			else if (line.equals("pupils")) { // Correction de contains en le remplaçant par equals plus adapté et correspondant aux autres conditions
-				pupilCount++;
-			}
-
-			line = reader.readLine();
+			writer.write("headache: " + headacheCount + "\n");
+			writer.write("rash: " + rashCount + "\n");
+			writer.write("dialated pupils: " + pupilCount + "\n");
+		} catch (Exception e) {
+			System.out.println("Erreur lors du traitement du fichier : " + e.getMessage());
+			/**
+			 * Exception handled by this try / catch
+			 */
 		}
-
-		reader.close();
-
-		FileWriter writer = new FileWriter("./result.out");
-		writer.write("headache: " + headacheCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
-		writer.close();
 	}
 }
